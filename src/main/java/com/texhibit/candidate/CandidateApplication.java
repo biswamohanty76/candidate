db5b87b359d0ab5378cf8d90106940c790c6707e
biswamohanty76 <70422715+biswamohanty76@users.noreply.github.com>
2023-08-02 23:05:19 +0530

    Update CandidateApplication.java

--- src/main/java/com/texhibit/candidate/CandidateApplication.java
+++ src/main/java/com/texhibit/candidate/CandidateApplication.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @OpenAPIDefinition
 public class CandidateApplication {
 
