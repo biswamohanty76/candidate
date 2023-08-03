d509c7aac3191cbbd65a0b65493324460671179a
biswamohanty76 <70422715+biswamohanty76@users.noreply.github.com>
2023-08-03 23:36:23 +0530

    Update CandidateService.java

--- src/main/java/com/texhibit/candidate/services/CandidateService.java
+++ src/main/java/com/texhibit/candidate/services/CandidateService.java
@@ -24,8 +24,8 @@ public class CandidateService {
     }
 
     public Candidate addCandidate(Candidate candidate) throws IOException {
-        String parseResume =  Parser.parse(candidate.getResumePath());
-        candidate.setResumeOrCV(parseResume);
+        // String parseResume =  Parser.parse(candidate.getResumePath());
+        // candidate.setResumeOrCV(parseResume);
         Candidate temp = candidateRepository.save(candidate);
 
         return temp;
